2b81c1f639354d44c532234873e75528c637405e
Jeremy Montes <jeremymontes@pursuit.org>
2023-09-07 11:21:04 -0400

    added h1 for testing

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Jeremy was here</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
