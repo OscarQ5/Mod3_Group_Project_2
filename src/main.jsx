67638ac161eb97c73c3f10b2fca82b25f8b857c4
Natyka Callwood <Natykacallwood@pursuit.org>
2023-09-11 12:05:39 -0400

    Added the browser router and renamed from profile page to about page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -5,6 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
 )
